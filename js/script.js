882732cfc41c7bcfc805302576f88a63935cf9f1
Alisse <alissecardoso5@gmail.com>
2023-11-21 16:32:05 -0300

    estilizando meu modal

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Função para rolar de volta ao topo da página
+function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
+
+// Exibir o botão "Voltar ao Topo"
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("Voltar").style.display = "flex";
+    } else {
+        document.getElementById("Voltar").style.display = "none";
+    }
+}
+
+// abrir modal   
+function showModal() {
+    document.getElementById("modal").style.display = "flex";
+}
+//fechar modal
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+
+
+
+// abrir login  
+function showLogin() {
+    document.getElementById("login").style.display = "flex";
+}
+//fechar login
+function closeLogin() {
+    document.getElementById("login").style.display = "none";
+}
+
+
